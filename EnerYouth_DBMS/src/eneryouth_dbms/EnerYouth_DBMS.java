d7f7ec6be01db9ef6dd40e300bea963a4ccaa12a
Aleju <aanglada@correo.ugr.es>
2025-11-03 18:03:34 +0100

    Añado proyecto de Java

---
+++ EnerYouth_DBMS/src/eneryouth_dbms/EnerYouth_DBMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eneryouth_dbms;
+
+/**
+ *
+ * @author alejandro
+ */
+public class EnerYouth_DBMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
