9608d8b784a1bae440e9a50fe2b9f8c7d78d70e7
Alejandro Anglada Álvarez <ale.anglada.alv@gmail.com>
2025-12-23 08:10:49 +0100

    Añado funcionalidad de borrado de filas de Instalación Energética. NOTA: Prevalece la funcionalidad semántica sobre la funcionalidad sintáctica (espero que me haya explicado bien); ver a partir de línea 106 en GestionRecursosEnergeticos.cpp.

--- src/GestionRecursosEnergeticos.cpp
+++ src/GestionRecursosEnergeticos.cpp
@@ -62,3 +62,69 @@ bool GestionRecursosEnergeticos::altaFuenteEnergetica(
 
     return retorno;
 }
+
+// La duplicidad de comentarios viene de copiar y pegar código para *seguir* cumpliendo el esquema ODBC de conexión.
+bool GestionRecursosEnergeticos::bajaFuenteEnergetica(const std::string& nombre, 
+                                                      const std::string& direccion){
+    // Valor de retorno por defecto: fallo. Se actualizará si todo va bien; y asumimos que no (hay más casos de
+    // fallo (aunque menos probables) que de éxito; menos overhead (aunque sea muy pequeño, sigue existiendo y
+    // hay que optimizar al máximo :P))
+    bool retorno = false;
+    // Para ver que efectivamente se hayan borrado filas. Si no, no hay error, pero quizá no se han pasado correcta-
+    // mente los parámetros.
+    SQLLEN filas_afectadas = 0;
+
+    // Comprobamos que la conexión esté establecida.
+    if (conexion.isConnected()) {
+        // Conexión
+        SQLHDBC conex = conexion.getConnection();
+        // Handler (nulo hasta inicializarlo)
+        SQLHSTMT handler = SQL_NULL_HSTMT;
+
+        // Inicializamos el handler:
+        SQLRETURN ret = SQLAllocHandle(SQL_HANDLE_STMT, conex, &handler);
+
+        if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+
+            // Definimos la sentencia SQL:
+            std::string sql_sentence = "DELETE FROM Instalacion_Energetica WHERE (";
+            sql_sentence += "Nombre_Fuente_Energetica = '" + nombre + "' AND ";
+            sql_sentence += "Direccion_Instalaciones = '" + direccion + "');";
+            // Ejecutamos la sentencia.
+            ret = SQLExecDirect(handler,
+                                (SQLCHAR*)sql_sentence.c_str(),
+                                SQL_NTS);
+            // Si no ha habido fallo:
+            if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+                // Contamos cuántas filas han sido afectadas
+                SQLRowCount(handler, &filas_afectadas);
+                // Si se ha borrado una fila (todo bien):
+                if (filas_afectadas == 1) {
+                    SQLEndTran(SQL_HANDLE_DBC, conex, SQL_COMMIT);
+                    retorno = true;
+                } 
+                // Si no se ha borrado fila (no tira error, pero semánticamente está mal por lo que
+                // devuelve false):
+                else {
+                    std::cout << "WARNING: No se ha borrado ninguna fila.\n";
+                    SQLEndTran(SQL_HANDLE_DBC, conex, SQL_ROLLBACK);
+                }
+
+            } else {
+                std::cerr << "ERROR: No se pudo borrar la fuente energética.\n";
+                SQLEndTran(SQL_HANDLE_DBC, conex, SQL_ROLLBACK);
+        }
+
+
+            // Liberamos el handler de sentencia.
+            SQLFreeHandle(SQL_HANDLE_STMT, handler);
+        } else {
+            std::cerr << "ERROR: No se pudo crear el handler de sentencia.\n";
+        }
+
+    } else {
+        std::cerr << "ERROR: Conexión no establecida correctamente. Compruebe sus credenciales.\nFinalizando...\n";
+    }
+
+    return retorno;
+}
