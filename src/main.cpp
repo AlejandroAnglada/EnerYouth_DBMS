96da768c4995cdf4dc5c7b891961a49a9bd9132b
Alejandro Anglada Álvarez <ale.anglada.alv@gmail.com>
2025-12-27 16:39:36 +0100

    Pequeñísima modificación para evitar error de compilación

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@
 #include <sqlext.h>
 #include <sqltypes.h>
 #include <string>
+// Incluímos con ruta relativa; da lo mismo pero por si acaso
 #include "../include/ConexionADB.h"
 #include "../include/GestionTransmisionDistribucion.h"
 #include "../include/GestionEmpleados.h"
+#include "../include/GestionRecursosEnergeticos.h"
 
 
 void crearTablas(ConexionADB &conexion, SQLHSTMT handler) {
@@ -264,7 +266,6 @@ void mostrarContenidoTablas(ConexionADB &conexion, SQLHSTMT handler) {
     SQLExecDirectA(handler, (SQLCHAR*)"SELECT * FROM Empleado;", SQL_NTS); 
     std::cout << "\n--Tabla Empleado:--\n";
     SQLVARCHAR DNI[10], Nombre[21], Apellidos[41], Telefono[21], Correo_Electronico[31], Posicion_Empresa[21];
-    SQLINTEGER Ventas, Incentivo;
     while (SQLFetch(handler) == SQL_SUCCESS) {
         SQLGetData(handler, 1, SQL_C_CHAR, DNI, sizeof(DNI), NULL);
         SQLGetData(handler, 2, SQL_C_CHAR, Nombre, sizeof(Nombre), NULL);
@@ -272,16 +273,12 @@ void mostrarContenidoTablas(ConexionADB &conexion, SQLHSTMT handler) {
         SQLGetData(handler, 4, SQL_C_CHAR, Telefono, sizeof(Telefono), NULL);
         SQLGetData(handler, 5, SQL_C_CHAR, Correo_Electronico, sizeof(Correo_Electronico), NULL);
         SQLGetData(handler, 6, SQL_C_CHAR, Posicion_Empresa, sizeof(Posicion_Empresa), NULL);
-        SQLGetData(handler, 7, SQL_C_LONG, &Ventas, 0, NULL);
-        SQLGetData(handler, 8, SQL_C_LONG, &Incentivo, 0, NULL);
         std::cout << "DNI:" << DNI << "\n";
         std::cout << "Nombre:" << Nombre << "\n";
         std::cout << "Apellidos:" << Apellidos << "\n";
         std::cout << "Telefono:" << Telefono << "\n";
         std::cout << "Correo_Electronico:" << Correo_Electronico << "\n";
         std::cout << "Posicion_Empresa:" << Posicion_Empresa << "\n";
-        std::cout << "Ventas:" << Ventas << "\n";
-        std::cout << "Incentivo:" << Incentivo << "\n";
     }
     SQLFreeStmt(handler, SQL_CLOSE);
 
@@ -701,9 +698,9 @@ void gestionEmpleados(GestionEmpleados &empleados) {
             }
             case 5: {
                 
-                std::vector<EmpleadoInfo> incentivo_empleados = empleados.incentivoParaEmpleados();
+                std::pair<std::vector<EmpleadoInfo>,int> incentivo_empleados = empleados.incentivoParaEmpleados();
                     std::cout << "Empleados encontrados:\n";
-                    for (const auto& e : incentivo_empleados) {
+                    for (const auto& e : incentivo_empleados.first) {
                         std::cout << "DNI: " << e.dni_empleado << "\n";
                         std::cout << "Nombre: " << e.nombre << "\n";
                         std::cout << "Apellidos: " << e.apellidos << "\n";
@@ -711,7 +708,7 @@ void gestionEmpleados(GestionEmpleados &empleados) {
                         std::cout << "Correo Electronico: " << e.correo_electronico << "\n";
                         std::cout << "Puesto: " << e.puesto << "\n";
                         std::cout << "Ventas:" << e.ventas << "\n";
-                        std::cout << "Incentivo: " << e.incentivo << "\n\n";
+                        std::cout << "Incentivo: " << incentivo_empleados.second << "\n\n";
                     }               
                      break;
             }
@@ -784,6 +781,13 @@ void crearTriggerVentas(ConexionADB &conexion, SQLHSTMT handler) {
 
 }
 
+// GESTIÓN DE RECURSOS ENERGÉTICOS: Pequeña interfaz para seleccionar qué funcionalidad(es) se desea seleccionar
+// de dicho subsistema. Se pasa como referencia una instancia de "GestionRecursosEnergeticos" para acceder a las
+// conexiones y, desde dicha instancia, llamar a todos los métodos pertinentes que quiera el usuario ejecutar.
+void gestionRecursosEnergeticos(GestionRecursosEnergeticos& gre){
+    
+}
+
 int main(int argc, char ** argv){
     std::string user, pwd;
     int opcion;
@@ -791,6 +795,7 @@ int main(int argc, char ** argv){
     // Declaramos el entorno y la conexión:
     SQLHSTMT handler;
     ConexionADB conexion;
+
     // Pedimos las credenciales al usuario para conectarse a la db:
     std::string dsn = "practbd"; // Nombre del DSN, puesto por defecto tal y como en la explicación del S1
     std::cout << "Esperando credenciales...\n";
